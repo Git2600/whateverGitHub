ebdda980c80fb2798f2e4bbcae9f336293af5e0a
Git2600 <ruffian2600@outlook.com>
2014-05-19 15:31:03 -0400

    lol

---
+++ Welcome.java
@@ -0,0 +1,10 @@
+/** This application program displays welcome to Java
+  * in a message Dialouge Box
+  **/
+public class Welcome {
+  public static void main(String[]args){
+    System.out.println("Welcome to Java");
+     System.out.println("Welcome to 1302");
+    
+  }
+}
