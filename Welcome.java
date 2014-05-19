7fa351775ecfac3a6dd9641a031568325e8e1ecf
Git2600 <ruffian2600@outlook.com>
2014-05-19 15:50:04 -0400

    2nd commit

--- Welcome.java
+++ Welcome.java
@@ -5,6 +5,8 @@ public class Welcome {
   public static void main(String[]args){
     System.out.println("Welcome to Java");
      System.out.println("Welcome to 1302");
+      System.out.println("Welcome to GitHub");
+        System.out.println("Git Hub Is Awesome");
     
   }
 }
